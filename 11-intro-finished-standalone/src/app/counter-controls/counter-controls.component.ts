3c6e8f7965a2a56fb00c2d31014a7671b0a3970a
Johan Tuitel <jtuitel@hotmail.com>
2024-05-02 10:58:35 +0200

    upgraded to angular 17

--- 11-intro-finished-standalone/src/app/counter-controls/counter-controls.component.ts
+++ 11-intro-finished-standalone/src/app/counter-controls/counter-controls.component.ts
@@ -1,7 +1,6 @@
 import { Component } from '@angular/core';
 import { Store } from '@ngrx/store';
-
-import { increment, decrement } from '../store/counter.actions';
+import { decrement, increment } from 'src/features/counter/counter.actions';
 
 @Component({
   selector: 'app-counter-controls',
